9d012413083da361219effbd9e20de5d953c91c9
hwroitzsch <hwroitzsch@gmail.com>
2016-11-24 19:35:42 +0100

    added test output

--- FileReader.py
+++ FileReader.py
@@ -5,7 +5,9 @@ class FileReader():
         super().__init__()
 
     def read_file(self, file_path):
+        print('reading file:', file_path)
         lines = []
         with open(file_path, mode='rU') as opened_file:
             lines = opened_file.read().splitlines()
+        print('file content is:', lines)
         return lines
--- app.py
+++ app.py
@@ -15,28 +15,31 @@ class App():
         self.file_reader = FileReader.FileReader()
         self.file_writer = FileWriter.FileWriter()
 
-    def parse(self, rst_file_path, bib_file_path):
+    def parse(self, rst_file_path):
+        print('called parse in app')
         rst_file_content = self.file_reader.read_file(rst_file_path)
         rst_file_content = self.rst_parser.add_raw_latex_rst_role(
             rst_file_content
         )
+        print('\n\nfile content with added rst role is:', rst_file_content)
         rst_file_content = self.rst_parser.parse(rst_file_content)
+        print('\n\nfinished rst file content is:', rst_file_content)
         self.file_writer.write(rst_file_path + '.out', rst_file_content)
 
 
 def print_help():
     print('call syntax:')
-    print('python <program> <rst file> <bib file>')
+    print('python <program> <rst file>')
 
 
 def main(params):
-    if len(params) != 3:
+    if len(params) != 2:
         print_help()
     else:
         rst_file_path = params[1]
-        bib_file_path = params[2]
         app = App()
-        app.parse(rst_file_path, bib_file_path)
+        print('App will work with file path:', rst_file_path)
+        app.parse(rst_file_path)
 
 
 if __name__ == '__main__':
